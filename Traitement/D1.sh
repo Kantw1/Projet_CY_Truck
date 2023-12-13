d12228398462d87a1a11a2fbd14bd570c26a925c
Kantw1 <fourrierquentin9@gmail.com>
2023-12-13 16:13:45 +0100

    fonction tri qui fonctionne mais pas encore le gnuplot

--- Traitement/D1.sh
+++ Traitement/D1.sh
@@ -1,3 +1,16 @@
-fichier_trajets="data.csv"
+fichier_trajets="Data/data.csv"
+fichier_resultats="Temp/resultat.txt"
 
-cat < data.txt | head -6 | cut -d' ' -f1-2
+#cat "$fichier_trajets" | cut -d';' -f6 | sort | uniq -c | sort -nr | head -n 10 > $fichier_resultats
+
+
+set terminal pngcairo enhanced font 'arial,10' size 800, 400
+set output 'Image/histogramme_horizontal.png'  # Chemin complet vers le dossier "images"
+set title 'Histogramme horizontal des données'
+set xlabel 'Nombre de trajets'
+set ylabel 'Conducteurs'
+set style fill solid
+set boxwidth 0.5
+set ytics nomirror
+set yrange [0:10] reverse
+plot 'Temp/resultat.txt' using 1:($0):($2) with boxes lc rgbcolor 'blue' notitle
