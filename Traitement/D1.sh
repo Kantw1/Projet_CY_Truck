057b777b87abaec5c2265e0ea29414b988fe3ff8
Kantw1 <fourrierquentin9@gmail.com>
2023-12-13 19:45:24 +0100

    changement de variable, bar à chager

--- Traitement/D1.sh
+++ Traitement/D1.sh
@@ -3,14 +3,19 @@ fichier_resultats="Temp/resultat.txt"
 
 #cat "$fichier_trajets" | cut -d';' -f6 | sort | uniq -c | sort -nr | head -n 10 > $fichier_resultats
 
-
+gnuplot -persist <<EOF
 set terminal pngcairo enhanced font 'arial,10' size 800, 400
-set output 'Image/histogramme_horizontal.png'  # Chemin complet vers le dossier "images"
+set output 'Image/histogramme_horizontal.png'  # Chemin complet vers le dossier "Image"
 set title 'Histogramme horizontal des données'
 set xlabel 'Nombre de trajets'
 set ylabel 'Conducteurs'
 set style fill solid
 set boxwidth 0.5
+set xtics nomirror
 set ytics nomirror
+set yrange [0:*]
 set yrange [0:10] reverse
-plot 'Temp/resultat.txt' using 1:($0):($2) with boxes lc rgbcolor 'blue' notitle
+
+plot '$fichier_resultats' using 1:0:ytic(2) with boxes lc rgbcolor 'blue' notitle
+EOF
+
