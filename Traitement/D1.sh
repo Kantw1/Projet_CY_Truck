5982a76914b535e085eed3969a63b6077d74c2ff
Kantw1 <fourrierquentin9@gmail.com>
2023-12-14 01:46:58 +0100

    traitement 1 presque terminé, affiche du graph et le tri effectué

--- Traitement/D1.sh
+++ Traitement/D1.sh
@@ -1,21 +1,19 @@
 fichier_trajets="Data/data.csv"
 fichier_resultats="Temp/resultat.txt"
+fichier_fusionne="Temp/fusion.txt"
 
-#cat "$fichier_trajets" | cut -d';' -f6 | sort | uniq -c | sort -nr | head -n 10 > $fichier_resultats
+#cat "$fichier_trajets" | cut -d';' -f6 | sort | uniq -c | sort -nr | head -n 10 | sort -k1,1n > $fichier_resultats
 
 gnuplot -persist <<EOF
 set terminal pngcairo enhanced font 'arial,10' size 800, 400
-set output 'Image/histogramme_horizontal.png'  # Chemin complet vers le dossier "Image"
+set output 'Image/histogramme_horizontal.png'
 set title 'Histogramme horizontal des données'
 set xlabel 'Nombre de trajets'
 set ylabel 'Conducteurs'
 set style fill solid
-set boxwidth 0.5
 set xtics nomirror
 set ytics nomirror
-set yrange [0:*]
-set yrange [0:10] reverse
+set yrange [0:10]
 
-plot '$fichier_resultats' using 1:0:ytic(2) with boxes lc rgbcolor 'blue' notitle
+plot '$fichier_resultats' using (\$1*0.5):0:(\$1*0.5):(0.3):yticlabels(3) with boxxyerrorbars lc rgbcolor 'blue' ti "Histogramme Horizontal"
 EOF
-
