1944de30f521c19e2d8ebc405e9c5fb8a5b3ee8a
matteo-dab <153624968+matteo-dab@users.noreply.github.com>
2023-12-19 09:46:22 +0100

    Update D2.sh

--- Traitement/D2.sh
+++ Traitement/D2.sh
@@ -2,18 +2,18 @@ fichier_trajets="Data/data.csv"
 fichier_resultats="Temp/resultat 2.txt"
 fichier_fusionne="Temp/fusion 2.txt"
 
-awk -F';' '{ distances[$6] += $3 } END { for (cond in distances) print cond, distances[cond] }' "$fichier_trajets" | sort -k2,2nr | head -n 10 | sort -k1,1n > "$fichier_resultats"
+awk -F';' '{ distances[$6] += $2 } END { for (cond in distances) print cond, distances[cond] }' "$fichier_trajets" | sort -k2,2nr | head -n 10 | sort -k1,1n > "$fichier_resultats"
 
 gnuplot -persist <<EOF
 set terminal pngcairo enhanced font 'arial,10' size 800, 400
-set output 'Image/histogramme_horizontal2.png'
+set output 'Image/histogramme_horizontal_2.png'
 set title 'Histogramme horizontal 2 des données'
-set xlabel 'Distance total parcourue'
+set xlabel 'Distance totale parcourue'
 set ylabel 'Nom des conducteurs'
 set style fill solid
 set xtics nomirror
 set ytics nomirror
 set yrange [0:10]
 
-plot '$fichier_resultats' using (\$1*0.5):0:(\$1*0.5):(0.3):yticlabels(3) with boxxyerrorbars lc rgbcolor 'blue' ti "Histogramme Horizontal2"
-EOF                   
+plot '$fichier_resultats' using 2:xtic(1) with boxes lc rgbcolor 'blue' ti "Histogramme Horizontal 2"
+EOF
