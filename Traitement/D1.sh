74681936e4ec0d1ee59bf6f4e6481ab34c5cc457
Kantw1 <fourrierquentin9@gmail.com>
2023-12-19 12:01:09 +0100

    rectifications

--- Traitement/D1.sh
+++ Traitement/D1.sh
@@ -4,7 +4,7 @@ fichier_fusionne="Temp/fusion.txt"
 
 awk -F ';' '!seen[$6,$1]++ { conducteurs[$6]++} END { for (diver in conducteurs) print conducteurs[diver], diver}' $fichier_trajets |sort -k1,1nr | head -n 10 | sort -k1,1n > $fichier_resultats
 #cat "$fichier_trajets" | cut -d';' -f6 | sort | uniq -c | sort -nr | head -n 10 | sort -k1,1n > $fichier_resultats bon ça fonctionne pas trop quoi
-
+#test!!! !
 gnuplot -persist <<EOF
 set terminal pngcairo enhanced font 'arial,10' size 800, 400
 set output 'Image/histogramme_horizontal.png'
--- Traitement/D2.sh
+++ Traitement/D2.sh
@@ -1,9 +1,7 @@
 fichier_trajets="Data/data.csv"
-fichier_resultats="Temp/resultat 2.txt"
-fichier_fusionne="Temp/fusion 2.txt"
-
-awk -F';' '{ distances[$6] += $2 } END { for (cond in distances) print cond, distances[cond] }' "$fichier_trajets" | sort -k2,2nr | head -n 10 | sort -k1,1n > "$fichier_resultats"
-
+fichier_resultats="Temp/resultat_2.txt"
+fichier_fusionne="Temp/fusion_2.txt"
+awk -F ';' '{ distances[$6] += $2 } END { for (cond in distances) print cond, distances[cond] }' $fichier_trajets | sort -k2,2nr | head -n 10 | sort -k1,1n > $fichier_resultats
 gnuplot -persist <<EOF
 set terminal pngcairo enhanced font 'arial,10' size 800, 400
 set output 'Image/histogramme_horizontal_2.png'
@@ -15,5 +13,5 @@ set xtics nomirror
 set ytics nomirror
 set yrange [0:10]
 
-plot '$fichier_resultats' using 2:xtic(1) with boxes lc rgbcolor 'blue' ti "Histogramme Horizontal 2"
+#plot '$fichier_resultats' using 2:xtic(1) with boxes lc rgbcolor 'blue' ti "Histogramme Horizontal 2"
 EOF
