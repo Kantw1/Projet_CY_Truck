49bbfa169a5603ac4f417f061e7b1d95179d86e9
matteo-dab <153624968+matteo-dab@users.noreply.github.com>
2023-12-19 09:13:29 +0100

    le script.sh est fini

--- Script.sh
+++ Script.sh
@@ -1,6 +1,7 @@
 #script schell
 
-#chmod 777 Script.sh
+chmod 777 Script.sh
+chmod 777 Traitement/D1.sh Traitement/D2.sh Traitement/T.sh Traitement/L.sh Traitement/S.sh 
 
 fichier1="Data/data.csv"
 
@@ -8,6 +9,38 @@ fichier1="Data/data.csv"
 afficher_aide() {
     echo "  -h, : En lancant ce programme vous avez le choix entre différents arguments 
     liés à différents traitements"
+}
+
+# Fonction pour le traitement D1
+traitement_D1() {
+    echo "Traitement D1"
+    # Ajoutez les commandes spécifiques pour le traitement D1
+    ./Traitement/D1.sh
+}
+
+# Fonction pour le traitement D2
+traitement_D2() {
+    echo "Traitement D2"
+    # Ajoutez les commandes spécifiques pour le traitement D2
+    ./Traitement/D2.sh
+}
+
+# Fonction pour le traitement L
+traitement_L() {
+    echo "Traitement L"
+    # Ajoutez les commandes spécifiques pour le traitement L
+}
+
+# Fonction pour le traitement T
+traitement_T() {
+    echo "Traitement T"
+    # Ajoutez les commandes spécifiques pour le traitement T
+}
+
+# Fonction pour le traitement S
+traitement_S() {
+    echo "Traitement S"
+    # Ajoutez les commandes spécifiques pour le traitement S
 }
 
 # Vérifier l'existence du fichier
@@ -26,24 +59,38 @@ else
     make build
     cd ..
 fi
+
+# Vérifier si l'argument -h est présent
+if [ "$1" == "-h" ]; then
+    afficher_aide
+    exit 0
+fi
+
 #vérifier si le dossier Temp existe
 if [ ! -d "Temp" ]; then
     mkdir "Temp"
 else
     rm -f Temp/*  #Supprimer tous les fichiers dans le dossier Temp si il existe
 fi
+
 #vérifier si le dossier Image existe
 if [ ! -d "Image" ]; then
     echo "creation du dossier image"
     mkdir "Image"
 fi
-# Vérifier si l'argument -h est présent
-if [ "$1" == "-h" ]; then
-    afficher_aide
-    exit 0
-fi
+
 # Mesurer le temps de début
 start_time=$(date +%s)
+
+# Vérifier et exécuter le traitement en fonction de l'argument fourni
+case "$1" in
+    "D1") traitement_D1 ;;
+    "D2") traitement_D2 ;;
+    "L")  traitement_L ;;
+    "T")  traitement_T ;;
+    "S")  traitement_S ;;
+    *)    echo "Argument invalide. Utilisez l'option -h pour afficher l'aide." ;;
+esac
 
 # Exécuter le programme avec les arguments appropriés
 "$executable" "$@"
@@ -53,5 +100,3 @@ end_time=$(date +%s)
 elapsed_time=$((end_time - start_time))
 
 echo "Le traitement a pris $elapsed_time secondes."
-#test
-# IL reste ca : le programme GnuPlot et choisir le traitement
