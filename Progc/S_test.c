76c7362cc1250470a20760f25873d98c93c771a3
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-01-16 23:57:57 +0100

    Update S_test.c

--- Progc/S_test.c
+++ Progc/S_test.c
@@ -212,7 +212,7 @@ void processStats(struct EtapeAVL* root) {
 
     // Afficher les statistiques et générer les données pour le graphique
     FILE* dataFile = fopen("Temp/Resultat_s2.txt", "w");
-    fprintf(dataFile, "#ID Distance_mini Distance_moyenne Distance_maxi\n");
+    fprintf(dataFile, "#ID Distance_mini Distance_moyenne Distance_maxi Distance_max-Distance_min\n");
     for (int i = 0; i < currentIndex; ++i) {
         fprintf(dataFile, "%.2lf %.2lf %.2lf %.2lf %.2lf\n", sortedData[i]->noeud->id_trajet, sortedData[i]->noeud->distance_min, sortedData[i]->noeud->distance_moyenne, sortedData[i]->noeud->distance_max, sortedData[i]->noeud->distance_max_min);
     }
