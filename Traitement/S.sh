e96bf90bd7aeb8d7be186d41d48b6c4b08cd1f6c
Kantw1 <fourrierquentin9@gmail.com>
2024-01-17 21:06:30 +0100

    fonction s.sh

--- Traitement/S.sh
+++ Traitement/S.sh
@@ -17,6 +17,6 @@ set yrange [0:1000]
 set xtics rotate by 45 offset -3,-1.5
 
 # Utilisez le bon nom de fichier 'Data/data_s.txt'
-plot '$fichier_resultats_test' using 2:4 with filledcurve lc rgbcolor 'blue' title "Shaded error region",\
-    ''using 1:(\$3):(\$1) smooth mcspline lc rgbcolor 'spring-green' title "Distance average (Km), 
+plot '$fichier_resultats_test' using 3:xtic(1) smooth mcspline lc rgbcolor 'spring-green' title "Distance average (Km)
 EOF
+#using ytic(2) with filledcurve lc rgbcolor 'blue' title "Shaded error region"
