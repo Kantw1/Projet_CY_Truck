b629d784945d9cd4625facbbbdaa10a1656b808f
Kantw1 <fourrierquentin9@gmail.com>
2024-01-17 13:58:12 +0100

    S.c

--- Traitement/S.sh
+++ Traitement/S.sh
@@ -1,6 +1,6 @@
 #création des fichiers nécessaires !
 fichier_trajets="Data/data.csv"
-fichier_resultats_test="Data/data_s.dat"
+fichier_resultats_test="Data/data_s.txt"
 
 #lancer le fichier .c
 
@@ -8,13 +8,13 @@ fichier_resultats_test="Data/data_s.dat"
 gnuplot -persist <<EOF
 set terminal pngcairo enhanced font 'arial,10' size 700, 600
 set output 'Image/histogramme_horizontal.png'
-set title 'Option -d1 : Nbroutes = f(Driver)'
-set xlabel 'NB ROUTES'
-set ylabel 'DRIVER NAMES'
-set style fill solid
-#set xtics nomirror #je crois qu'il y a pas besoin de nomirror
-#set ytics nomirror
-set yrange [-1:10]
+set title "Error on y represented by filledcurve shaded region"
+set xlabel "Time (sec)" 
+set ylabel "Rate" 
+set grid xtics mxtics ytics mytics
+set log y
+Shadecolor = "#80E0A080"
 
-plot '$fichier_resultats_test' using (\$1*0.5):0:(\$1*0.5):(0.4):yticlabels(sprintf("%s %s", stringcolumn(2), stringcolumn(3))) with boxxyerrorbars lc rgbcolor 'spring-green' notitle
+# Utilisez le bon nom de fichier 'Data/data_s.txt'
+plot $fichier_resultats_test using (\$2):2:(\$3) with filledcurve fc rgb Shadecolor title "Shaded error region", using 2:1 smooth mcspline lw 2 title "Monotonic spline through data"
 EOF
