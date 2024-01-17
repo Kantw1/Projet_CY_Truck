5db56bd0b1aa5cd95bf611eb72a1d649acf0c98f
Kantw1 <fourrierquentin9@gmail.com>
2024-01-17 14:31:34 +0100

    script S.sh

--- Traitement/S.sh
+++ Traitement/S.sh
@@ -1,20 +1,20 @@
-#création des fichiers nécessaires !
+#!/bin/bash
+# Création des fichiers nécessaires !
 fichier_trajets="Data/data.csv"
 fichier_resultats_test="Data/data_s.txt"
 
-#lancer le fichier .c
+# Lancer le fichier .c
 
-# utilisation de gnuplot
+# Utilisation de gnuplot
 gnuplot -persist <<EOF
 set terminal pngcairo enhanced font 'arial,10' size 700, 600
-set output 'Image/histogramme_horizontal.png'
-set title "Error on y represented by filledcurve shaded region"
-set xlabel "Time (sec)" 
-set ylabel "Rate" 
-set grid xtics mxtics ytics mytics
-set log y
-Shadecolor = "#80E0A080"
+set output 'Image/histogramme_horizontal_5.png'
+set title "Option -S : Distance = f(Route)"
+set ylabel 'DISTANCE (km)'
+#set xtics nomirror #je crois qu'il y a pas besoin de nomirror
+#set ytics nomirror
+set yrange [0:1000]
 
 # Utilisez le bon nom de fichier 'Data/data_s.txt'
-plot $fichier_resultats_test using (\$2):2:(\$3) with filledcurve fc rgb Shadecolor title "Shaded error region", using 2:1 smooth mcspline lw 2 title "Monotonic spline through data"
+plot '$fichier_resultats_test' using 2:xtic(1) lc rgbcolor 'spring-green' with boxes notitle
 EOF
