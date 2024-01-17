4e23d2b429d760721ceb7e8c791e53271b1b235b
Kantw1 <fourrierquentin9@gmail.com>
2024-01-17 13:19:11 +0100

    S.h add

---
+++ Traitement/S.sh
@@ -0,0 +1,22 @@
+#création des fichiers nécessaires !
+fichier_trajets="Data/data.csv"
+fichier_resultats="Temp/resultat.txt"
+fichier_resultats_test="Data/data_s.dat"
+
+#calcul du nombre de trajets de chaque conducteur
+awk -F ';' '!seen[$6,$1]++ { conducteurs[$6]++} END { for (diver in conducteurs) print conducteurs[diver], diver}' $fichier_trajets |sort -k1,1nr | head -n 10 | sort -k1,1n > $fichier_resultats
+
+# utilisation de gnuplot
+gnuplot -persist <<EOF
+set terminal pngcairo enhanced font 'arial,10' size 700, 600
+set output 'Image/histogramme_horizontal.png'
+set title 'Option -d1 : Nbroutes = f(Driver)'
+set xlabel 'NB ROUTES'
+set ylabel 'DRIVER NAMES'
+set style fill solid
+#set xtics nomirror #je crois qu'il y a pas besoin de nomirror
+#set ytics nomirror
+set yrange [-1:10]
+
+plot '$fichier_resultats_test' using (\$1*0.5):0:(\$1*0.5):(0.4):yticlabels(sprintf("%s %s", stringcolumn(2), stringcolumn(3))) with boxxyerrorbars lc rgbcolor 'spring-green' notitle
+EOF
