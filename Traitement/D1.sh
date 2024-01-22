93690813a9442c28c5629c0156dfab2d642d328a
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-01-22 16:51:05 +0100

    Update D1.sh

--- Traitement/D1.sh
+++ Traitement/D1.sh
@@ -2,10 +2,10 @@
 fichier_trajets="Data/data.csv"
 fichier_resultats="Temp/resultat.txt"
 
-#calcul du nombre de trajets de chaque conducteur
+# Tri des données utilisant awk pour additionner le nombre de trajet faits par conducteur, et renvoie des 10 conducteurs avec le plus de trajets
 awk -F ';' '!seen[$6,$1]++ { conducteurs[$6]++} END { for (diver in conducteurs) print conducteurs[diver], diver}' $fichier_trajets |sort -k1,1nr | head -n 10 | sort -k1,1n > $fichier_resultats
 
-# utilisation de gnuplot
+# Utilisation de gnuplot pour la création d'un graphe
 gnuplot -persist <<EOF
 set terminal pngcairo enhanced font 'arial,10' size 700, 600
 set output 'Image/histogramme_horizontal_1.png'
@@ -17,5 +17,6 @@ set yrange [-1:10]
 
 plot '$fichier_resultats' using (\$1*0.5):0:(\$1*0.5):(0.4):yticlabels(sprintf("%s %s", stringcolumn(2), stringcolumn(3))) with boxxyerrorbars lc rgbcolor 'spring-green' notitle
 EOF
+# Utilisation de chmod pour donner les droits absolues à l'accès au graphe de sortie en png
 chmod 777 Image/histogramme_horizontal_1.png
 xdg-open Image/histogramme_horizontal_1.png
