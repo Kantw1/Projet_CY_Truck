8d195821fc25ef6abf8b1d6a6d1813619c43fcb7
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-01-22 16:53:16 +0100

    Update D1.sh

--- Traitement/D1.sh
+++ Traitement/D1.sh
@@ -1,8 +1,8 @@
-#création des fichiers nécessaires !
+# Création des fichiers nécessaires !
 fichier_trajets="Data/data.csv"
 fichier_resultats="Temp/resultat.txt"
 
-# Tri des données utilisant awk pour additionner le nombre de trajet faits par conducteur, et renvoie des 10 conducteurs avec le plus de trajets
+# Tri des données utilisant awk pour additionner le nombre de trajets faits par conducteur, et renvoie des 10 conducteurs avec le plus de trajets
 awk -F ';' '!seen[$6,$1]++ { conducteurs[$6]++} END { for (diver in conducteurs) print conducteurs[diver], diver}' $fichier_trajets |sort -k1,1nr | head -n 10 | sort -k1,1n > $fichier_resultats
 
 # Utilisation de gnuplot pour la création d'un graphe
@@ -13,6 +13,7 @@ set title 'Option -d1 : Nbroutes = f(Driver)'
 set xlabel 'NB ROUTES'
 set ylabel 'DRIVER NAMES'
 set style fill solid
+# Mise en place de la plage de yrange sur 10 en plage maximal
 set yrange [-1:10]
 
 plot '$fichier_resultats' using (\$1*0.5):0:(\$1*0.5):(0.4):yticlabels(sprintf("%s %s", stringcolumn(2), stringcolumn(3))) with boxxyerrorbars lc rgbcolor 'spring-green' notitle
