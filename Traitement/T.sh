9e3c92891457ee92635f2c260dc3f40e6adf678b
Quentin Fourrier <131240800+Kantw1@users.noreply.github.com>
2024-01-22 16:52:56 +0100

    Update T.sh

--- Traitement/T.sh
+++ Traitement/T.sh
@@ -3,23 +3,15 @@ fichier_resultats="Temp/resultat_T.txt"
 fichier_resultats2="Temp/resultat_T2.txt"
 fichier_resultats3="Temp/Resultat_T3.txt"
 fichier_resultats4="Temp/resultat_T4.txt"
+fichier_resultats5="Temp/resultat_T5.txt"
 fichier_resultats_test="Data/data_t.txt"
 
-# Mise en place des données utiles dans un 1er fichier, ensuite utiliser dans le T.c plutôt que de prendre le data.csv
-cut -d';' -f3,4,6 "$fichier_trajets" | tail -n +2 > $fichier_resultats #head -n 6000 à enlever
-awk -F';' '{
-  if ($1 == $2) {
-     #Si les colonnes 1 et 2 sont identiques, supprimer la colonne 2
-    print $1 FS $3
-  } else {
-    # Sinon, appliquer la commande awk pour supprimer la dernière colonne et trier
-    for (i=1; i<=NF-1; i++) print $i FS $NF
-  }
-}' $fichier_resultats | sort -t';' -k1,1 > $fichier_resultats2
-awk -F';' '{for(i=1;i<=NF-1;i++) print $i FS $NF}' $fichier_resultats | sort -t';' -k1,1 > $fichier_resultats2
-sed -i 's/$/;/g' $fichier_resultats2
+cut -d';' -f3,4,1 "$fichier_trajets" | tail -n +2 | head -n 1 > $fichier_resultats #head -n 6000 à enlever
+awk -F';' '{print $1 FS $2; print $1 FS $3}' $fichier_resultats | sort -t';' -k2,2 > $fichier_resultats2
+cut -d ';' -f1,2 "$fichier_resultats" | sort -t';' -k2,2 > $fichier_resultats3
+#sed -i 's/$/;/g' $fichier_resultats2
 ./Progc/prog_t
-sort -t';' -k1,1 "$fichier_resultats3" > $fichier_resultats4
+#sort -t';' -k1,1 "$fichier_resultats4" > $fichier_resultats5
 #max_value=$(awk -F';' 'NR==1 {max=$2} $2>max {max=$2} END {print max}' "$fichier_resultats4")
 #maw_value2=$(awk 'BEGIN {rounded = int((ENVIRON["max_value"] + 999) / 1000) * 1000; printf "%.0f\n", rounded}')
 
@@ -38,10 +30,9 @@ set bmargin 5
 set style histogram clustered gap 1
 set style fill solid 0.8 border
 
-plot '$fichier_resultats4' using (\$0):2:xticlabel(1) with boxes lc rgbcolor 'spring-green' notitle,\
-    '' using (\$0+0.2):(\$3) with boxes lc rgbcolor 'dark-green' notitle
-#plot for [COL=2:3] '$fichier_resultats_test' using COL:xticlabels(1) with boxes 
+#plot '$fichier_resultats4' using (\$0):2:xticlabel(1) with boxes lc rgbcolor 'spring-green' notitle,\
+#    '' using (\$0+0.2):(\$3) with boxes lc rgbcolor 'dark-green' notitle
 EOF
 
-chmod 777 Image/histogramme_horizontal_4.png
-xdg-open Image/histogramme_horizontal_4.png
+#chmod 777 Image/histogramme_horizontal_4.png
+#xdg-open Image/histogramme_horizontal_4.png
